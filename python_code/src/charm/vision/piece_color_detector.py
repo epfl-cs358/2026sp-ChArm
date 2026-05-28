af102bc39e7566d20ba23b15ee40cbc287a5d274
jibben <allan.burnier@gmail.com>
2026-05-28 12:53:27 +0200

    changed some defualts value for the vision

--- python_code/src/charm/vision/piece_color_detector.py
+++ python_code/src/charm/vision/piece_color_detector.py
@@ -39,8 +39,8 @@ def compute_piece_brightness_score(cell_image: np.ndarray) -> float:
 def detect_piece_colors(
     cells: list[SquareCell],
     occupancy_results: list[OccupancyResult],
-    white_threshold: float = 80,
-    black_threshold: float = 80,
+    white_threshold: float = 90,
+    black_threshold: float = 90,
 ) -> list[PieceColorResult]:
     """
     Only classify occupied cells.
